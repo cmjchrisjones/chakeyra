d11f22d919d3014033c535db4f3e94d3eaf9e424
Chris Jones <cmj.chris.jones@googlemail.com>
2020-11-28 23:10:48 +0000

    trying to remove having the auth token in env settings

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,5 @@ WORKDIR /opt/app
 COPY . /opt/app
  
 EXPOSE 3000
-# RUN npm config set unsafe-perm true
-# # CMD ["npm", "start"]
 
-ENTRYPOINT ["nodemon", "./src/server/server.js"]
+ENTRYPOINT ["npm", "start"]
--- src/server/chat-capture.js
+++ src/server/chat-capture.js
@@ -28,24 +28,49 @@ class UserData {
 }
 
 let users = new Array();
+const AUTH_TOKEN = '';
+
+function getTwitchAuthToken() {
+  return axios({
+    method: 'post',
+    url: `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&grant_type=client_credentials&scope=user:read:email`
+  })
+    .then(res => {
+      console.log(res.data);
+      console.log(AUTH_TOKEN)
+      return AUTH_TOKEN;
+    })
+    .then(_ => {
+      console.log(AUTH_TOKEN);
+      return AUTH_TOKEN;
+    })
+}
+
 
 function getUserProfilePic(userId) {
+  let bearerToken = '';
+  if (bearerToken = '') {
+    bearerToken = getTwitchAuthToken();
+  }
+
+  console.log("bearer token: " + bearerToken);
   return axios({
     method: "get",
     url: `https://api.twitch.tv/helix/users?id=${userId}`,
     headers: {
-      Authorization: `Bearer ${process.env.TWITCH_CHANNEL_AUTH_TOKEN}`,
+      Authorization: `Bearer ${bearerToken}`,
       'Content-Type': 'application/json',
       'Client-ID': process.env.TWITCH_CLIENT_ID
     }
   })
     .then(res => {
       if (res.data) {
+        console.log(res.data);
         const body = res.data;
         const userData = body.data.length > 1 ? body.data : body.data[0];
         if (userData) {
           let profileUrl = userData.profile_image_url;
-          var profileImageUrl = profileUrl.replace('300x300', '70x70'); 
+          var profileImageUrl = profileUrl.replace('300x300', '70x70');
           return profileImageUrl;
         }
       }
@@ -59,7 +84,7 @@ function connect(server) {
   ComfyJS.onChat = chatHandler;
   ComfyJS.onCommand = commandHandler;
 
-  ComfyJS.Init('cmjchrisjones');
+  ComfyJS.Init(process.env.TWITCH_CHANNEL_ID);
 }
 
 function startGame() {
